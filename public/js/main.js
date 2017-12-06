bd7bebb5fb2b40fbb2f571194e7f397aaafe3451
Landon <landonwjohnson@gmail.com>
2017-12-05 17:26:20 -0700

    just started the project

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+function loadImage(url){
+    return new Promise(resolve => {
+        
+    })
+}
+
+const canvas = document.getElementById('screen');
+
+const context = canvas.getContext('2d');
+
+context.fillRect(0, 0, 50, 50);
