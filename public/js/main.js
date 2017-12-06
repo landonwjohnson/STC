1a56902a20362109982fe9eaa8a918d2a7adea58
Landon <landonwjohnson@gmail.com>
2017-12-05 17:39:44 -0700

    935m

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 function loadImage(url){
     return new Promise(resolve => {
-        
-    })
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image)
+        });
+        image.src = url;
+    });
 }
 
 const canvas = document.getElementById('screen');
@@ -9,3 +13,14 @@ const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 context.fillRect(0, 0, 50, 50);
+
+loadImage('/img/tiles.png')
+.then(image => {
+    const sprites = new SpriteSheet(image);
+    sprites.define('')
+    context.drawImage(image, 
+        0, 0 , 
+        16, 16,
+        32, 32,
+        16, 16);
+})
